a5d586ca23e22e0e7ef9a71542bb1b4f986d2638
tim <tim@balter.io>
2021-01-18 12:47:56 +1000

    Removed check for day greater than 3 HA-45

--- scripts/bin-day/collection_type.py
+++ scripts/bin-day/collection_type.py
@@ -7,16 +7,15 @@ def collection_type():
     weeks = WasteRequests()
     today = date.today()
 
-    if today.weekday() > 3:  # if this thursday has passed, we only care about next week
-        week_starting = today + timedelta((0 - today.weekday()) % 7)  # calculates next monday
+    week_starting = today + timedelta((0 - today.weekday()) % 7)  # calculates next monday
 
-        for week in weeks.get_week_request()['result']['records']:
-            if week['WEEK_STARTING'] == week_starting.strftime('%d/%m/%Y'):
-                week_zone = week['ZONE']
+    for week in weeks.get_week_request()['result']['records']:
+        if week['WEEK_STARTING'] == week_starting.strftime('%d/%m/%Y'):
+            week_zone = week['ZONE']
 
-                bin_type = 'Recycling' if weeks.get_day_request()['result']['records'][0]['ZONE'] == week_zone else 'Garden waste'
+            bin_type = 'Recycling' if weeks.get_day_request()['result']['records'][0]['ZONE'] == week_zone else 'Garden waste'
 
-                print(f'Landfill & {bin_type}')
+            print(f'Landfill & {bin_type}')
 
 
 if __name__ == '__main__':
