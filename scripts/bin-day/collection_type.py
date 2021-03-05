e151329b02f5c45dd247a2bbce6f99985dbc0c6e
tim <tim@balter.io>
2021-03-06 09:57:06 +1000

    Refactored collection type to produce correct bin type HA-50

--- scripts/bin-day/collection_type.py
+++ scripts/bin-day/collection_type.py
@@ -4,6 +4,11 @@ from waste_resources.requests import WasteRequests
 
 
 def collection_type():
+    """
+    Uses the Brisbane City Council API to determine the bin collection type each week
+    https://www.data.brisbane.qld.gov.au/data/dataset/a3d075b9-70d7-40b3-a693-70db2a415765/resource/10699d57-3a21-4b07-b819-38ae62a9bffc/download/wca_2019-12-02.txt
+    """
+
     weeks = WasteRequests()
     today = date.today()
     bin_type = None
@@ -11,12 +16,9 @@ def collection_type():
     week_starting = today + timedelta((0 - today.weekday()) % 7)  # calculates next monday
 
     for week in weeks.get_week_request()['result']['records']:
-        week_zone = week['ZONE']
-
-        bin_type = 'Recycling' if weeks.get_day_request()['result']['records'][0]['ZONE'] == week_zone else 'Garden waste'
-
-    print(f'Landfill & {bin_type}')
-
-
-if __name__ == '__main__':
-    collection_type()
+        if week['WEEK_STARTING'] == week_starting.strftime('%-d/%m/%Y'):
+            week_zone = week['ZONE']
+            bin_type = 'Recycling' if weeks.get_day_request()['result']['records'][0]['ZONE'] == \
+                week_zone else 'Garden waste'
+            print(f'Landfill & {bin_type}')
+            break
