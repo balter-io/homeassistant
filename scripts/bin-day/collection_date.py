75ba50b2c1aed57e4ce3f7b24c3b8f1f2507d83f
tim <tim@balter.io>
2021-03-17 12:35:41 +1000

    Refactored collection date code to return correct date HA-55

--- scripts/bin-day/collection_date.py
+++ scripts/bin-day/collection_date.py
@@ -6,14 +6,12 @@ def collection_date():
     """ Determine the bin collection date. """
 
     today = date.today()
-    if date.weekday(today) > 2:		# normal collection day is thursday (day 3 of a zero-indexed week)
-        today += timedelta((0 - today.weekday()) % 7)   # if thursday has passed, we only care about next week
+    next_date = today + timedelta((3 - today.weekday()) % 7)  # set the next collection date (next thursday)
 
-        next_date = today + timedelta((3 - today.weekday()) % 7)  # set the collection date to next thursday
-        collection = next_date.strftime('%d-%b-%Y')
-
-        print(collection)
-        return collection
+    if date.weekday(today) > 3:		# normal collection day is thursday (day 3 of a zero-indexed week)
+        print(next_date.strftime('%d-%b-%Y'))
+    else:
+        print(next_date.strftime('%d-%b-%Y'))
 
 
 if __name__ == '__main__':
