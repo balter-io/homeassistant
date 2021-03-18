95f8967ff8546850abe2fb6b3170578c18573a4e
tim <tim@balter.io>
2021-03-18 15:40:19 +1000

    Refactored to display next collection date on the collection day HA-56

--- scripts/bin-day/collection_date.py
+++ scripts/bin-day/collection_date.py
@@ -6,10 +6,15 @@ def collection_date():
     """ Determine the bin collection date. """
 
     today = date.today()
-    next_date = today + timedelta((3 - today.weekday()) % 7)  # set the next collection date (next thursday)
+    next_date = today + timedelta((3 - today.weekday()) % 7)  # calculate the next collection date (next thursday)
 
-    if date.weekday(today) > 3:		# normal collection day is thursday (day 3 of a zero-indexed week)
+    if next_date == today:
+        next_date = today + timedelta(7)  # if today is the collection date, add 7 days to get next weeks date
         print(next_date.strftime('%d-%b-%Y'))
+
+    elif date.weekday(today) > 3:		# evaluates whether thursday has passed (day 3 of a zero-indexed week)
+        print(next_date.strftime('%d-%b-%Y'))
+
     else:
         print(next_date.strftime('%d-%b-%Y'))
 
