c51521143f2bbd350bdfcb542b16343f45f8f283
tim <tim@balter.io>
2021-06-27 19:01:41 +1000

    Refactored logic to continue showing correct bin type HA-67

--- scripts/bin-day/collection_date.py
+++ scripts/bin-day/collection_date.py
@@ -10,13 +10,8 @@ def collection_date():
 
     if next_date == today:
         next_date = today + timedelta(7)  # if today is the collection date, add 7 days to get next weeks date
-        print(next_date.strftime('%d-%b-%Y'))
-
-    elif date.weekday(today) > 3:		# evaluates whether thursday has passed (day 3 of a zero-indexed week)
-        print(next_date.strftime('%d-%b-%Y'))
-
-    else:
-        print(next_date.strftime('%d-%b-%Y'))
+    print(next_date.strftime('%d-%b-%Y'))
+    return next_date
 
 
 if __name__ == '__main__':
