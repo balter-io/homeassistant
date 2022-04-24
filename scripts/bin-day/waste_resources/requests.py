c50aef1e92e99392f85ea2cdd6516a22723ee717
tim <tim@balter.io>
2022-04-24 13:09:55 +1000

    updated hassos, hacs & mushroom card HA-75

--- scripts/bin-day/waste_resources/requests.py
+++ scripts/bin-day/waste_resources/requests.py
@@ -4,9 +4,9 @@ import os
 
 class WasteRequests:
 
-    def __init__(self):
-        self.day_request = os.getenv('day')
-        self.week_request = os.getenv('week')
+    def __init__(self, day_url, week_url):
+        self.day_request = day_url
+        self.week_request = week_url
 
     def get_day_request(self):
         return requests.get(self.day_request).json()
