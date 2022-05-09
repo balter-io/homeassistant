e4478ff090d3d1d7f7870fd8cc48bc49cbabe4f6
tim <tim@balter.io>
2022-05-09 16:58:23 +1000

    maintenance

--- scripts/bin-day/collection_type.py
+++ scripts/bin-day/collection_type.py
@@ -1,5 +1,4 @@
 #!usr/bin/env python3
-import sys
 from datetime import date, datetime, timedelta
 from waste_resources.requests import WasteRequests
 
@@ -10,7 +9,7 @@ def collection_type():
     https://www.data.brisbane.qld.gov.au/data/dataset/a3d075b9-70d7-40b3-a693-70db2a415765/resource/10699d57-3a21-4b07-b819-38ae62a9bffc/download/wca_2019-12-02.txt
     """
 
-    weeks = WasteRequests(sys.argv[1], sys.argv[2])
+    weeks = WasteRequests()
     bin_type = None
 
     for week in weeks.get_week_request()['result']['records']:
@@ -27,7 +26,7 @@ def collection_type():
             break
 
         if date.today() < collection_date:
-            bin_type = 'Recycling' if weeks.get_week_request()['result']['records'][0]['ZONE'] == \
+            bin_type = 'Recycling' if weeks.get_day_request()['result']['records'][0]['ZONE'] == \
                 zone else 'Garden waste'
             print(f'Landfill and {bin_type}')
             break
@@ -35,3 +34,4 @@ def collection_type():
 
 if __name__ == '__main__':
     collection_type()
+
